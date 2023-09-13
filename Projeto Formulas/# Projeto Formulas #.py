6a82339658d2a7b6847f52831b9e0f390c17cf92
Matheus Meloni <63019098+MatMeloni@users.noreply.github.com>
2023-09-13 16:32:51 -0300

    Update # Projeto Formulas #.py

--- Projeto Formulas/# Projeto Formulas #.py
+++ Projeto Formulas/# Projeto Formulas #.py
@@ -2,5 +2,12 @@
 
 import math as m
 
-forma=str(input('qual a forma do calculo:'))
-print('a forma é:',forma)
+forma=str(input("qual a forma do calculo:"))
+print("a forma é:",forma)
+
+mylist=["triangulo","quadrado","cubo","esfera","losangulo","prisma"]
+if forma in mylist:
+    print(forma)
+else:
+    print("não existe formula")
+
