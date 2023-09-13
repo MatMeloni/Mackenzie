8d29fe070af0d7049d72087a6ca8571c9c4a7dde
Matheus Meloni <63019098+MatMeloni@users.noreply.github.com>
2023-09-13 13:15:27 -0300

    Add files via upload

---
+++ Area Do Trapezio.py
@@ -0,0 +1,7 @@
+#Area do Trapezio
+
+B1=float(input('Valor da Base Maior: '))
+B2=float(input('Valor da Base Menor: '))
+H=float(input('Valor da Altura do Trapezio: '))
+Area=((B1+B2)*H)/2
+print('A Area do Trapezio é de: ',Area)
---
+++ Area e Circuferencia do circulo.py
@@ -0,0 +1,7 @@
+import math
+
+r=float(input("valor do raio:"))
+a=math.pi*math.pow(r,2)
+c=2*math.pi*r
+print("valor da area",a)
+print("valor da circunferencia",c)
